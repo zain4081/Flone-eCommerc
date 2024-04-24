79ab44be431245f1289b4266acee096de1b9463f
zain <zain.abbas@thehexaa.com>
2024-04-24 11:50:35 +0500

    add file for blog posts and comments queryies

---
+++ src/services/blogApi.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const blogApi = createApi({
+  reducerPath: 'blogApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/blog/' }),
+  endpoints: (builder) => ({
+    submitComment: builder.mutation({
+        query:({data, access_token})=>{
+            console.log('q userdata',data)
+            console.log('q access_token',access_token)
+            return{
+                url:`posts/${data.post}/comments/`,
+                method: 'POST',
+                body: data,
+                headers: {
+                    'authorization': `Bearer ${access_token}`,
+                },
+            }
+        }
+      }),
+    getComments: builder.mutation({
+        query: (postId, token) => ({
+          url: `posts/${postId}/`,
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        }),
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useSubmitCommentMutation, useGetCommentsMutation } = blogApi;
