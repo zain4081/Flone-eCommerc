3b0456affe69e083f4c6a1ace3f8cc0089f14f6d
zain <zain.abbas@thehexaa.com>
2024-06-10 12:13:48 +0500

    add docstring to views.py in notifications app

--- zain-backend/notification/views.py
+++ zain-backend/notification/views.py
@@ -13,27 +13,28 @@ from django.db import transaction
     
 class UserNotifications(APIView):
     """
-    API view for user profile management.
+    API view to retrieve unread notifications for the authenticated user.
+
+    This view handles HTTP GET requests to retrieve unread notifications
+    for the authenticated user. Additionally, an update is sent via WebSocket
+    to notify the client of the unread notification count.
     """
     renderer_classes = [UserRenderer]
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         """
-        Handles GET request for user profile.
+        Handles HTTP GET requests to retrieve unread notifications.
 
-        Args:
-        - request: Request object containing the user profile data.
-        - format: Optional format suffix.
-
-        Returns:
-        - Response: Response object with user profile data.
+        This method retrieves all unread notifications for the authenticated user.
+        If there are unread notifications, it sends a WebSocket message to update
+        the unread notification count for the user.
         """
-        
         notifications = Notification.objects.filter(read=False, recipient=request.user)
         serializer = NotificationSerializer(notifications, many=True)
-        
+
         if notifications:
-              # Send update count via WebSocket
+            # Send update count via WebSocket
             channel_layer = get_channel_layer()
             async_to_sync(channel_layer.group_send)(
                 f'notifications_{request.user.id}',
@@ -42,24 +43,39 @@ class UserNotifications(APIView):
                     'message': 'update',
                     'unread_count': notifications.count(),
                 }
-                )
+            )
         data = {
             "notifications": serializer.data,
             "unread_count": notifications.count(),
         }
         return Response(data, status=status.HTTP_200_OK)
-    
+
 
 class AddNotification(APIView):
-    renderer_classes= [UserRenderer]
-    permission_classes= [IsAuthenticated]
-    
+    """
+    API view to add a new notification.
+
+    This view handles HTTP POST requests to create a new notification.
+    Additionally, it sends a WebSocket message to notify the client
+    of the new notification and update the unread notification count.
+    """
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
+        """
+        Handles HTTP POST requests to add a new notification.
+
+        This method validates the request data, creates a new notification,
+        and sends a WebSocket message to notify the recipient of the new
+        notification and update the unread notification count.
+        """
         serializer = AddNotifcation(data=request.data)
         serializer.is_valid(raise_exception=True)
         notification = serializer.save()
         unread_count = Notification.objects.filter(read=False, recipient=notification.recipient.id).count()
-        if(notification):
+        
+        if notification:
             # Send notification via WebSocket
             channel_layer = get_channel_layer()
             async_to_sync(channel_layer.group_send)(
@@ -69,17 +85,34 @@ class AddNotification(APIView):
                     'message': notification.message,
                     'unread_count': unread_count,
                 }
-                )
-            return Response({'msg':'Notify Successfully'},
-                        status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,
-                        status=status.HTTP_400_BAD_REQUEST)
+            )
+            return Response({'msg': 'Notify Successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class EditNotificationReadStatus(APIView):
+    """
+    API view to mark notifications as read for the authenticated user.
+
+    This view handles HTTP PUT requests to update the read status of notifications
+    for the authenticated user. All unread notifications are marked as read.
+    Additionally, an update is sent via WebSocket to notify the client of the change
+    in unread notification count.
+
+    Methods:
+        put(request):
+            Marks all unread notifications as read of requesting user and sends a WebSocket update.
+    """
     renderer_classes = [UserRenderer]
     permission_classes = [IsAuthenticated]
 
     def put(self, request):
+        """
+        Handles HTTP PUT requests to mark notifications as read.
+
+        This method updates all unread notifications for the authenticated user
+        to be marked as read. If the operation is successful, it sends a WebSocket
+        message to update the unread notification count for the user.
+        """
         try:
             # Ensure atomic database operations
             with transaction.atomic():
