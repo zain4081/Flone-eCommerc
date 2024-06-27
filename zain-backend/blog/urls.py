b5365bdbb2718c8178a4d21fcc44a9e46839a983
zain <zain.abbas@thehexaa.com>
2024-06-27 14:17:35 +0500

    add schema.py file for graph ql views and also add url

--- zain-backend/blog/urls.py
+++ zain-backend/blog/urls.py
@@ -4,6 +4,12 @@ URL configuration for the blog app.
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog import views
+from graphene_django.views import GraphQLView
+from .schema import schema
+
+urlpatterns = [
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+]
 
 # Create a router for API endpoints
 router = DefaultRouter()
@@ -22,7 +28,8 @@ router.register(r'post-search', views.PostDocumentViewSet, basename='post-search
 urlpatterns = [
     path('', include(router.urls)),
     path('user-votes/<int:post_id>', views.UserVoteView.as_view(), name='user-votes'),
-    path('first-post-id/', views.FirstPostIdView.as_view(), name='first-post-id'),
+    # path('first-post-id/', views.FirstPostIdView.as_view(), name='first-post-id'),
+    path('fisrt-post-id/', GraphQLView.as_view(graphiql=True, schema=schema)),
     
     # # new urls
     # path('posts-s/<int:post_id>/', views.CommentApiView.as_view(), name='c'),
