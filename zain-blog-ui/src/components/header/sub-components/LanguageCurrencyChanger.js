78ad33927304ca3f9bb803e4cf642d3fd53f0644
zain <zain.abbas@thehexaa.com>
2024-07-03 17:53:12 +0500

    Add Multi currency & multi language base on use location

--- zain-blog-ui/src/components/header/sub-components/LanguageCurrencyChanger.js
+++ zain-blog-ui/src/components/header/sub-components/LanguageCurrencyChanger.js
@@ -1,19 +1,68 @@
 import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
-import { setCurrency } from "../../../store/slices/currency-slice"
+import { setCurrency } from "../../../store/slices/currency-slice";
 
-const LanguageCurrencyChanger = ({ currency }) => {
+const LanguageCurrencyChanger = ({ defaultCurrency = "USD" }) => {
   const { i18n } = useTranslation();
   const dispatch = useDispatch();
-  const changeLanguageTrigger = e => {
+  const currency = useSelector(state => state.currency); // Adjust selector according to your Redux state structure
+  const [userCountry, setUserCountry] = useState(null);
+
+  useEffect(() => {
+    fetchUserLocation();
+  }, []);
+
+  const fetchUserLocation = async () => {
+    try {
+      const response = await fetch('https://ipinfo.io/json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch user location');
+      }
+      const data = await response.json();
+      setUserCountry(data.country);
+      // Dispatch action to set currency based on user's country
+      dispatch(setCurrency(getCurrencyForCountry(data.country)));
+    } catch (error) {
+      console.error('Error fetching user location:', error);
+      // Fallback to default currency/language on error
+      setUserCountry(null); // or handle specific error case
+    }
+  };
+
+  useEffect(() => {
+    // Set default language to English
+    i18n.changeLanguage("en");
+  }, [i18n]);
+
+  const changeLanguageTrigger = (e) => {
     const languageCode = e.target.value;
     i18n.changeLanguage(languageCode);
   };
 
-  const setCurrencyTrigger = e => {
+  const setCurrencyTrigger = (e) => {
     const currencyName = e.target.value;
     dispatch(setCurrency(currencyName));
+  };
+
+  const getInitialCurrency = () => {
+    // Default to USD if user's country is not detected or mapped
+    return currency.currencyName || defaultCurrency;
+  };
+
+  const getCurrencyForCountry = (countryCode) => {
+    // Add logic to map user's country to currency
+    switch (countryCode) {
+      case "US":
+        return "USD";
+      case "FR":
+        return "EUR";
+      case "DE":
+        return "EUR";
+      default:
+        return defaultCurrency; // Default currency passed as prop
+    }
   };
 
   return (
@@ -22,28 +71,28 @@ const LanguageCurrencyChanger = ({ currency }) => {
         <span>
           {i18n.resolvedLanguage === "en"
             ? "English"
-            : i18n.resolvedLanguage === "fn"
+            : i18n.resolvedLanguage === "fr"
             ? "French"
             : i18n.resolvedLanguage === "de"
-            ? "Germany"
-            : ""}{" "}
+            ? "German"
+            : "Unknown Language"}{" "}
           <i className="fa fa-angle-down" />
         </span>
         <div className="lang-car-dropdown">
           <ul>
             <li>
-              <button value="en" onClick={e => changeLanguageTrigger(e)}>
+              <button value="en" onClick={(e) => changeLanguageTrigger(e)}>
                 English
               </button>
             </li>
             <li>
-              <button value="fn" onClick={e => changeLanguageTrigger(e)}>
+              <button value="fr" onClick={(e) => changeLanguageTrigger(e)}>
                 French
               </button>
             </li>
             <li>
-              <button value="de" onClick={e => changeLanguageTrigger(e)}>
-                Germany
+              <button value="de" onClick={(e) => changeLanguageTrigger(e)}>
+                German
               </button>
             </li>
           </ul>
@@ -51,22 +100,34 @@ const LanguageCurrencyChanger = ({ currency }) => {
       </div>
       <div className="same-language-currency use-style">
         <span>
-          {currency.currencyName} <i className="fa fa-angle-down" />
+          {getInitialCurrency()} <i className="fa fa-angle-down" />
         </span>
         <div className="lang-car-dropdown">
           <ul>
             <li>
-              <button value="USD" onClick={e => setCurrencyTrigger(e)}>
+              <button
+                value="USD"
+                onClick={(e) => setCurrencyTrigger(e)}
+                className={getInitialCurrency() === "USD" ? "active" : ""}
+              >
                 USD
               </button>
             </li>
             <li>
-              <button value="EUR" onClick={e => setCurrencyTrigger(e)}>
+              <button
+                value="EUR"
+                onClick={(e) => setCurrencyTrigger(e)}
+                className={getInitialCurrency() === "EUR" ? "active" : ""}
+              >
                 EUR
               </button>
             </li>
             <li>
-              <button value="GBP" onClick={e => setCurrencyTrigger(e)}>
+              <button
+                value="GBP"
+                onClick={(e) => setCurrencyTrigger(e)}
+                className={getInitialCurrency() === "GBP" ? "active" : ""}
+              >
                 GBP
               </button>
             </li>
@@ -81,7 +142,7 @@ const LanguageCurrencyChanger = ({ currency }) => {
 };
 
 LanguageCurrencyChanger.propTypes = {
-  currency: PropTypes.shape({}),
+  defaultCurrency: PropTypes.string.isRequired,
 };
 
 export default LanguageCurrencyChanger;
